9f29001039357d92824044b1e948c104a896941c
simone.rainieri <simone.rainieri@gmail.com>
2019-02-12 12:40:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,9 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:dennis_ritchie][:languages] = ["Assembly"]
+     return programmer_hash
 end
+
 
 def adding_to_dennis
 	programmer_hash = 

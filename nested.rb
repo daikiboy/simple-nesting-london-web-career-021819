d2fc42ea223e77e1803e3f761d0b2f4128370b1b
simone.rainieri <simone.rainieri@gmail.com>
2019-02-12 12:42:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     programmer_hash[:dennis_ritchie][:languages] = ["Assembly"]
-     return programmer_hash
 end
 
 
@@ -129,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+        programmer_hash[:dennis_ritchie][:languages] = ["Assembly"]
+     return programmer_hash
 
 end
